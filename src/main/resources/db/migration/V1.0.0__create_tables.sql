3367cd63df533b8191d1966953702dd5dac1547b
KenDahle <ken.dahle@nav.no>
2024-07-09 11:48:21 +0200

    TOB-3845 Tester om det er kontakt med krav db

--- src/main/resources/db/migration/V1.0.0__create_tables.sql
+++ src/main/resources/db/migration/V1.0.0__create_tables.sql
@@ -1,34 +1,41 @@
-drop table if exists krav;
-create table "utleggstrekk"
+drop table if exists hei;
+create table "hei"
 (
-    id                     bigserial primary key,
-    trekkid_ske            text,
-    trekkversjon_ske       smallint,
-    trekkopprettet         Timestamp,
-    trekkpliktig           text,
-    skyldner               text,
-    trekkstatus            text,
-    startPeriode           text,
-    sluttPeriode           text,
-    trekkbeloep            Double,
-    trekkprosent           Double,
-    kidnummer              text,
-    kontonummer            text,
-    corr_id                text,
-    tidspunkt_mottatt      timestamp,
-    tidspunkt_sendt_os     timestamp null,
-    tidspunkt_siste_status timestamp NOT NULL DEFAULT NOW(),
-    tidspunkt_opprettet    timestamp NOT NULL DEFAULT NOW()
-);
-
-drop table if exists;
-create table "midlertidigstans"
-(
-    id             bigserial primary key,
-    utleggstrekkid bigint,
-    startPeriode   text,
-    sluttPeriode   text
-);
-
-create index if not exists idxutleggstrekkid_nav on midlertidigstans (utleggstrekkid);
-create index if not exists idxtrekkid_ske on utleggstrekk (trekkid_ske);
+    id bigserial,
+    navn text,
+    melding text
+)
+-- drop table if exists krav;
+-- create table "utleggstrekk"
+-- (
+--     id                     bigserial primary key,
+--     trekkid_ske            text,
+--     trekkversjon_ske       smallint,
+--     trekkopprettet         Timestamp,
+--     trekkpliktig           text,
+--     skyldner               text,
+--     trekkstatus            text,
+--     startPeriode           text,
+--     sluttPeriode           text,
+--     trekkbeloep            Double,
+--     trekkprosent           Double,
+--     kidnummer              text,
+--     kontonummer            text,
+--     corr_id                text,
+--     tidspunkt_mottatt      timestamp,
+--     tidspunkt_sendt_os     timestamp null,
+--     tidspunkt_siste_status timestamp NOT NULL DEFAULT NOW(),
+--     tidspunkt_opprettet    timestamp NOT NULL DEFAULT NOW()
+-- );
+--
+-- drop table if exists;
+-- create table "midlertidigstans"
+-- (
+--     id             bigserial primary key,
+--     utleggstrekkid bigint,
+--     startPeriode   text,
+--     sluttPeriode   text
+-- );
+--
+-- create index if not exists idxutleggstrekkid_nav on midlertidigstans (utleggstrekkid);
+-- create index if not exists idxtrekkid_ske on utleggstrekk (trekkid_ske);
