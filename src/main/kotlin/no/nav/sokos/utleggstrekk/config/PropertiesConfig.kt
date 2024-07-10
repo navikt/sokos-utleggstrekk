d52622ad08072aec51e80dc9ceb4fa56f91a744f
Thea Marie Alnæs <thea.marie.alnes@nav.no>
2024-07-10 12:04:40 +0200

    TOB-3845 Hadde en typo i userrole/adminrole

--- src/main/kotlin/no/nav/sokos/utleggstrekk/config/PropertiesConfig.kt
+++ src/main/kotlin/no/nav/sokos/utleggstrekk/config/PropertiesConfig.kt
@@ -16,87 +16,95 @@ import no.nav.sokos.utleggstrekk.httpClient
 import java.io.File
 
 object PropertiesConfig {
+    private val defaultProperties =
+        ConfigurationMap(
+            mapOf(
+                "NAIS_APP_NAME" to "sokos-ske-krav",
+                "NAIS_NAMESPACE" to "okonomi",
+            ),
+        )
+    private val localDevProperties =
+        ConfigurationMap(
+            "APPLICATION_PROFILE" to Profile.LOCAL.toString(),
+            "POSTGRES_HOST" to "dev-pg.intern.nav.no",
+            "POSTGRES_PORT" to "5432",
+            "POSTGRES_NAME" to "sokos-utleggstrekk",
+            "SKE_REST_URL" to "https://api-test.sits.no/api/utleggstrekk/v1/",
+            "USE_AUTHENTICATION" to "false",
+        )
 
-	private val defaultProperties = ConfigurationMap(
-		mapOf(
-			"NAIS_APP_NAME" to "sokos-ske-krav",
-			"NAIS_NAMESPACE" to "okonomi",
-		)
-	)
-	private val localDevProperties = ConfigurationMap(
-		"APPLICATION_PROFILE" to Profile.LOCAL.toString(),
-		"POSTGRES_HOST" to "dev-pg.intern.nav.no",
-		"POSTGRES_PORT" to "5432",
-		"POSTGRES_NAME" to "sokos-utleggstrekk",
-		"SKE_REST_URL" to "https://api-test.sits.no/api/utleggstrekk/v1/",
-		"USE_AUTHENTICATION" to "false",
-	)
+    private val devProperties = ConfigurationMap(mapOf("APPLICATION_PROFILE" to Profile.DEV.toString()))
+    private val prodProperties = ConfigurationMap(mapOf("APPLICATION_PROFILE" to Profile.PROD.toString()))
 
-	private val devProperties = ConfigurationMap(mapOf("APPLICATION_PROFILE" to Profile.DEV.toString()))
-	private val prodProperties = ConfigurationMap(mapOf("APPLICATION_PROFILE" to Profile.PROD.toString()))
+    private val config =
+        when (System.getenv("NAIS_CLUSTER_NAME") ?: System.getProperty("NAIS_CLUSTER_NAME")) {
+            "dev-fss" -> ConfigurationProperties.systemProperties() overriding EnvironmentVariables() overriding devProperties overriding defaultProperties
+            "prod-fss" -> ConfigurationProperties.systemProperties() overriding EnvironmentVariables() overriding prodProperties overriding defaultProperties
+            else ->
+                ConfigurationProperties.systemProperties() overriding EnvironmentVariables() overriding
+                    ConfigurationProperties.fromOptionalFile(
+                        File("defaults.properties"),
+                    ) overriding localDevProperties overriding defaultProperties
+        }
 
-	private val config = when (System.getenv("NAIS_CLUSTER_NAME") ?: System.getProperty("NAIS_CLUSTER_NAME")) {
-		"dev-fss" -> ConfigurationProperties.systemProperties() overriding EnvironmentVariables() overriding devProperties overriding defaultProperties
-		"prod-fss" -> ConfigurationProperties.systemProperties() overriding EnvironmentVariables() overriding prodProperties overriding defaultProperties
-		else ->
-			ConfigurationProperties.systemProperties() overriding EnvironmentVariables() overriding ConfigurationProperties.fromOptionalFile(
-				File("defaults.properties")
-			) overriding localDevProperties overriding defaultProperties
-	}
+    enum class Profile {
+        LOCAL,
+        DEV,
+        PROD,
+    }
 
-	enum class Profile {
-		LOCAL, DEV, PROD
-	}
-	fun isLocal() = Configuration().profile == Profile.LOCAL
-	operator fun get(key: String): String = config[Key(key, stringType)]
-	fun getOrEmpty(key: String): String = config.getOrElse(Key(key, stringType), "")
+    fun isLocal() = Configuration().profile == Profile.LOCAL
 
-	data class Configuration(
-		val naisAppName: String = get("NAIS_APP_NAME"),
-		val profile: Profile = Profile.valueOf(this["APPLICATION_PROFILE"]),
-	)
+    operator fun get(key: String): String = config[Key(key, stringType)]
 
-	data class MaskinportenClientConfig(
-		val clientId: String = getOrEmpty("MASKINPORTEN_CLIENT_ID"),
-		val authorityEndpoint: String = getOrEmpty("MASKINPORTEN_WELL_KNOWN_URL"),
-		val rsaKey: RSAKey? = RSAKey.parse(getOrEmpty("MASKINPORTEN_CLIENT_JWK")),
-		val scopes: String = getOrEmpty("MASKINPORTEN_SCOPES"),
-	) : JwtConfig(authorityEndpoint)
+    fun getOrEmpty(key: String): String = config.getOrElse(Key(key, stringType), "")
 
-	@Serializable
-	data class OpenIdConfiguration(
-		@SerialName("jwks_uri")
-		val jwksUri: String,
-		@SerialName("issuer")
-		val issuer: String,
-		@SerialName("token_endpoint")
-		val tokenEndpoint: String,
-	)
+    data class Configuration(
+        val naisAppName: String = get("NAIS_APP_NAME"),
+        val profile: Profile = Profile.valueOf(this["APPLICATION_PROFILE"]),
+    )
 
-	data class SKEConfig(
-		val skeRestUrl: String = getOrEmpty("SKE_REST_URL")
-	)
+    data class MaskinportenClientConfig(
+        val clientId: String = getOrEmpty("MASKINPORTEN_CLIENT_ID"),
+        val authorityEndpoint: String = getOrEmpty("MASKINPORTEN_WELL_KNOWN_URL"),
+        val rsaKey: RSAKey? = RSAKey.parse(getOrEmpty("MASKINPORTEN_CLIENT_JWK")),
+        val scopes: String = getOrEmpty("MASKINPORTEN_SCOPES"),
+    ) : JwtConfig(authorityEndpoint)
 
-	data class PostgresConfig(
-		val host: String = getOrEmpty("POSTGRES_HOST"),
-		val port: String = getOrEmpty("POSTGRES_PORT"),
-		val name: String = getOrEmpty("POSTGRES_NAME"),
-		val username: String = getOrEmpty("POSTGRES_USERNAME").trim(),
-		val password: String = getOrEmpty("POSTGRES_PASSWORD").trim(),
-		val vaultMountPath: String = getOrEmpty("VAULT_MOUNTPATH"),
-	) {
-		val jdbcUrl: String = "jdbc:postgresql://$host:$port/$name"
-		val adminUser = "$name--admin"
-		val user = "$name--user"
-	}
+    @Serializable
+    data class OpenIdConfiguration(
+        @SerialName("jwks_uri")
+        val jwksUri: String,
+        @SerialName("issuer")
+        val issuer: String,
+        @SerialName("token_endpoint")
+        val tokenEndpoint: String,
+    )
 
-	open class JwtConfig(private val wellKnownUrl: String) {
+    data class SKEConfig(
+        val skeRestUrl: String = getOrEmpty("SKE_REST_URL"),
+    )
 
-		val openIdConfiguration: OpenIdConfiguration by lazy {
-			runBlocking {
-		httpClient.get(wellKnownUrl).body()
+    data class PostgresConfig(
+        val host: String = getOrEmpty("POSTGRES_HOST"),
+        val port: String = getOrEmpty("POSTGRES_PORT"),
+        val name: String = getOrEmpty("POSTGRES_NAME"),
+        val username: String = getOrEmpty("POSTGRES_USERNAME").trim(),
+        val password: String = getOrEmpty("POSTGRES_PASSWORD").trim(),
+        val vaultMountPath: String = getOrEmpty("VAULT_MOUNTPATH"),
+    ) {
+        val jdbcUrl: String = "jdbc:postgresql://$host:$port/$name"
+        val adminUser = "$name-admin"
+        val user = "$name-user"
+    }
 
-			}
-		}
-	}
+    open class JwtConfig(
+        private val wellKnownUrl: String,
+    ) {
+        val openIdConfiguration: OpenIdConfiguration by lazy {
+            runBlocking {
+                httpClient.get(wellKnownUrl).body()
+            }
+        }
+    }
 }
--- src/main/kotlin/no/nav/sokos/utleggstrekk/database/PostgresDataSource.kt
+++ src/main/kotlin/no/nav/sokos/utleggstrekk/database/PostgresDataSource.kt
@@ -7,44 +7,47 @@ import no.nav.sokos.utleggstrekk.config.PropertiesConfig
 import no.nav.vault.jdbc.hikaricp.HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration
 import org.flywaydb.core.Flyway
 
-
 object PostgresDataSource {
     private val postgresConfig: PropertiesConfig.PostgresConfig = PropertiesConfig.PostgresConfig()
     private val logger = KotlinLogging.logger {}
 
-    fun postgresMigrate(role: String = postgresConfig.adminUser, dataSource: HikariDataSource = dataSource(role)){
-      logger.info { "Flyway migration" }
-        Flyway.configure()
+    fun postgresMigrate(dataSource: HikariDataSource = dataSource(postgresConfig.adminUser)) {
+        logger.info { "Flyway migration" }
+        Flyway
+            .configure()
             .dataSource(dataSource)
-            .initSql("""SET ROLE "$role"""")
+            .initSql("""SET ROLE "${ postgresConfig.adminUser}"""")
             .load()
             .migrate()
-      logger.info { "Migration finished" }
+        logger.info { "Migration finished" }
     }
 
-   fun dataSource(role: String = postgresConfig.user): HikariDataSource =
-        if (PropertiesConfig.isLocal())
-            HikariDataSource(hikariConfig())
-        else createHikariDataSourceWithVaultIntegration(
-            hikariConfig(),
-            postgresConfig.vaultMountPath,
-            role
-        )
-
-    private fun hikariConfig() = HikariConfig().apply {
-        minimumIdle = 1
-        maxLifetime = 30000
-        maximumPoolSize = 4
-        connectionTimeout = 10000
-        isAutoCommit = false
-        idleTimeout = 10000
-        //connectionTestQuery = "SELECT * FROM ${dbConfig.testTable} LIMIT 1"
-        jdbcUrl = postgresConfig.jdbcUrl
-        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
-        validate()
+    fun dataSource(role: String = postgresConfig.user): HikariDataSource =
         if (PropertiesConfig.isLocal()) {
-            username = postgresConfig.username
-            password = postgresConfig.password
+            HikariDataSource(hikariConfig())
+        } else {
+            createHikariDataSourceWithVaultIntegration(
+                hikariConfig(),
+                postgresConfig.vaultMountPath,
+                role,
+            )
         }
-    }
+
+    private fun hikariConfig() =
+        HikariConfig().apply {
+            minimumIdle = 1
+            maxLifetime = 30000
+            maximumPoolSize = 4
+            connectionTimeout = 10000
+            isAutoCommit = false
+            idleTimeout = 10000
+            // connectionTestQuery = "SELECT * FROM ${dbConfig.testTable} LIMIT 1"
+            jdbcUrl = postgresConfig.jdbcUrl
+            transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+            validate()
+            if (PropertiesConfig.isLocal()) {
+                username = postgresConfig.username
+                password = postgresConfig.password
+            }
+        }
 }
