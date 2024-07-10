25cc2fdb15d67b87241000fcb4550605342d7fc3
KenDahle <ken.dahle@nav.no>
2024-07-10 11:30:07 +0200

    TOB-3845 oppdatert flyway med tabeller  for utleggstrekk

--- src/main/resources/db/migration/V1.0.0__create_tables.sql
+++ src/main/resources/db/migration/V1.0.0__create_tables.sql
@@ -1,41 +1,34 @@
-drop table if exists hei;
-create table "jada"
+drop table if exists krav;
+create table "utleggstrekk"
 (
-    id bigserial,
-    navn text,
-    melding text
+    id                     bigserial primary key,
+    trekkid_ske            text,
+    trekkversjon_ske       smallint,
+    trekkopprettet         Timestamp,
+    trekkpliktig           text,
+    skyldner               text,
+    trekkstatus            text,
+    startPeriode           text,
+    sluttPeriode           text,
+    trekkbeloep            decimal,
+    trekkprosent           decimal,
+    kidnummer              text,
+    kontonummer            text,
+    corr_id                text,
+    tidspunkt_mottatt      timestamp,
+    tidspunkt_sendt_os     timestamp null,
+    tidspunkt_siste_status timestamp NOT NULL DEFAULT NOW(),
+    tidspunkt_opprettet    timestamp NOT NULL DEFAULT NOW()
 );
--- drop table if exists krav;
--- create table "utleggstrekk"
--- (
---     id                     bigserial primary key,
---     trekkid_ske            text,
---     trekkversjon_ske       smallint,
---     trekkopprettet         Timestamp,
---     trekkpliktig           text,
---     skyldner               text,
---     trekkstatus            text,
---     startPeriode           text,
---     sluttPeriode           text,
---     trekkbeloep            Double,
---     trekkprosent           Double,
---     kidnummer              text,
---     kontonummer            text,
---     corr_id                text,
---     tidspunkt_mottatt      timestamp,
---     tidspunkt_sendt_os     timestamp null,
---     tidspunkt_siste_status timestamp NOT NULL DEFAULT NOW(),
---     tidspunkt_opprettet    timestamp NOT NULL DEFAULT NOW()
--- );
---
--- drop table if exists;
--- create table "midlertidigstans"
--- (
---     id             bigserial primary key,
---     utleggstrekkid bigint,
---     startPeriode   text,
---     sluttPeriode   text
--- );
---
--- create index if not exists idxutleggstrekkid_nav on midlertidigstans (utleggstrekkid);
--- create index if not exists idxtrekkid_ske on utleggstrekk (trekkid_ske);
+
+drop table if exists midlertidigstans;
+create table "midlertidigstans"
+(
+    id             bigserial primary key,
+    utleggstrekkid bigint,
+    startPeriode   text,
+    sluttPeriode   text
+);
+
+create index if not exists idxutleggstrekkid_nav on midlertidigstans (utleggstrekkid);
+create index if not exists idxtrekkid_ske on utleggstrekk (trekkid_ske);
