44c79361e344bcdcbcc8f92c0dd054b50db9b823
KenDahle <ken.dahle@nav.no>
2024-07-11 13:57:34 +0200

    TOB-3845 LAgrer midlertidig stans sammen med kravene

--- src/main/kotlin/no/nav/sokos/utleggstrekk/database/Repository.kt
+++ src/main/kotlin/no/nav/sokos/utleggstrekk/database/Repository.kt
@@ -31,7 +31,7 @@ object Repository {
     fun Connection.saveAllNewUtleggstrekk(
         trekkListe: List<Utleggstrekk>,
     ) {
-        val prepStmt =
+        val prepStmt1 =
             prepareStatement(
                 """
                 insert into utleggstrekk (
@@ -51,25 +51,46 @@ object Repository {
                 ) values (?,?,?,TO_TIMESTAMP(?, 'YYYY-MM-DD"T"HH24:MI:SS.MSZ'),?,?,?,?,?,?,?,?,?)
                 """.trimIndent(),
             )
+        val prepStmt2 =
+            prepareStatement(
+                """
+                insert into midlertidigstans (
+                trekksekvensnr, 
+                startperiode, 
+                sluttperiode
+                ) values (?, ?, ?)        
+               """.trimIndent(),
+            )
         trekkListe.forEach {
             val trekkBelop = it.trekkbeloep?.trekkbeloep
             val trekkProsent = it.trekkprosent?.trekkprosent
-            prepStmt.setInt(1, it.sekvensnummer)
-            prepStmt.setString(2, it.trekkid)
-            prepStmt.setInt(3, it.trekkversjon)
-            prepStmt.setString(4, it.opprettet)
-            prepStmt.setString(5, it.trekkpliktig)
-            prepStmt.setString(6, it.skyldner)
-            prepStmt.setString(7, it.trekkstatus)
-            prepStmt.setString(8, it.startPeriode)
-            prepStmt.setString(9, it.sluttPeriode)
-            prepStmt.setDouble(10, trekkBelop ?: 0.0)
-            prepStmt.setDouble(11, trekkProsent ?: 0.0)
-            prepStmt.setString(12, it.kidnummer)
-            prepStmt.setString(13, it.kontonummer)
-            prepStmt.addBatch()
+            prepStmt1.setInt(1, it.sekvensnummer)
+            prepStmt1.setString(2, it.trekkid)
+            prepStmt1.setInt(3, it.trekkversjon)
+            prepStmt1.setString(4, it.opprettet)
+            prepStmt1.setString(5, it.trekkpliktig)
+            prepStmt1.setString(6, it.skyldner)
+            prepStmt1.setString(7, it.trekkstatus)
+            prepStmt1.setString(8, it.startPeriode)
+            prepStmt1.setString(9, it.sluttPeriode)
+            prepStmt1.setDouble(10, trekkBelop ?: 0.0)
+            prepStmt1.setDouble(11, trekkProsent ?: 0.0)
+            prepStmt1.setString(12, it.kidnummer)
+            prepStmt1.setString(13, it.kontonummer)
+            prepStmt1.addBatch()
+
+            if (!it.midlertidigStans.isNullOrEmpty()) {
+                it.midlertidigStans.forEach { stans ->
+                    prepStmt2.setInt(1, it.sekvensnummer)
+                    prepStmt2.setString(2, stans.startPeriode ?: "ikke satt")
+                    prepStmt2.setString(3, stans.sluttPeriode ?: "ikke satt")
+                    prepStmt2.addBatch()
+                }
+            }
         }
-        prepStmt.executeBatch()
+        prepStmt1.executeBatch()
+        prepStmt2.executeBatch()
         commit()
     }
+
 }
