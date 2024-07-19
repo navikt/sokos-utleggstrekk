3c15b356131534f3ba07631aee9bd153bdef52ab
KenDahle <ken.dahle@nav.no>
2024-07-20 00:26:16 +0200

    TOB-3845 kotlinifisert litt mer

--- src/main/kotlin/no/nav/sokos/utleggstrekk/mq/ShowQueueDepth.kt
+++ src/main/kotlin/no/nav/sokos/utleggstrekk/mq/ShowQueueDepth.kt
@@ -2,9 +2,15 @@ package no.nav.sokos.utleggstrekk.mq
 
 import com.ibm.mq.MQEnvironment
 import com.ibm.mq.MQQueueManager
+import com.ibm.mq.constants.CMQC.MQCA_Q_NAME
+import com.ibm.mq.constants.CMQC.MQIA_CURRENT_Q_DEPTH
 import com.ibm.mq.constants.MQConstants
 import com.ibm.mq.headers.pcf.PCFMessage
 import com.ibm.mq.headers.pcf.PCFMessageAgent
+import java.lang.String
+import java.util.regex.Pattern
+import kotlin.also
+
 
 class ShowAllQueueDepth() {
     init {
@@ -15,7 +21,7 @@ class ShowAllQueueDepth() {
         MQEnvironment.password = "YVqijM6XK94l4Uav4MGOtt"
     }
 
-    fun allLocalQueueDepths():Array<PCFMessage> {
+    fun allLocalQueueDepths():List<kotlin.String> {
         val qmgr = MQQueueManager("MQLS01")
         val pcfCmd = PCFMessage(MQConstants.MQCMD_INQUIRE_Q)
         val agent = PCFMessageAgent()
@@ -24,18 +30,16 @@ class ShowAllQueueDepth() {
         pcfCmd.addParameter(MQConstants.MQIA_Q_TYPE, MQConstants.MQQT_LOCAL)
         pcfCmd.addFilterParameter(MQConstants.MQIA_CURRENT_Q_DEPTH, MQConstants.MQCFOP_GREATER, 0)
 
-        val pcfResponse = agent.send(pcfCmd)
+        val pcfResponse = agent.send(pcfCmd).asList()
 
 
-        for (index in pcfResponse.indices) {
-            val response = pcfResponse[index]
+        return pcfResponse.filterNotNull()
+            .filter { !Pattern.matches("^SYSTEM.*$", String.valueOf(it.getParameterValue(MQCA_Q_NAME))) }
+            .filter { !Pattern.matches("^AMK.*$", String.valueOf(it.getParameterValue(MQCA_Q_NAME))) }
+            .filter { !Pattern.matches("^AMQ.*$", String.valueOf(it.getParameterValue(MQCA_Q_NAME))) }
+            .map { pcfMessage ->
+                String.valueOf("KøNavn: ${pcfMessage.getParameterValue(MQCA_Q_NAME)}, Kødybde: ${pcfMessage.getParameterValue(MQIA_CURRENT_Q_DEPTH)}").also(::println)
+            }
 
-            println(
-                response.getParameterValue(MQConstants.MQCA_Q_NAME).toString() + "     "
-                        + response.getParameterValue(MQConstants.MQIA_CURRENT_Q_DEPTH)
-            )
-        }
-
-        return pcfResponse
     }
 }
