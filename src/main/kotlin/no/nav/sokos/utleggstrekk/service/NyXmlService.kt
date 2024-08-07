d452e83a48c9976a3531a996456d4b34025fa742
Thea Marie Alnæs <thea.marie.alnes@nav.no>
2024-08-07 11:21:07 +0200

    TOB-3845 Lagt inn mer XML greier med forklaringer fra Endre. Trenger at Endre legger inn "sats" i XML

--- src/main/kotlin/no/nav/sokos/utleggstrekk/service/NyXmlService.kt
+++ src/main/kotlin/no/nav/sokos/utleggstrekk/service/NyXmlService.kt
@@ -4,30 +4,32 @@ import kotlinx.serialization.Serializable
 import nl.adaptivity.xmlutil.serialization.XmlElement
 import nl.adaptivity.xmlutil.serialization.XmlSerialName
 import no.nav.sokos.utleggstrekk.database.model.TrekkTable
+import java.lang.String.valueOf
 
 class NyXmlService {
     fun createNyTrekkXMLObjects(trekk: TrekkTable) {
-        val sats =
-            when {
-                trekk.trekkbelop == null -> trekk.trekkprosent!!
-                else -> trekk.trekkbelop
+        val (sats, kodeTrekkAlternativ) =
+            if (trekk.trekkbelop == null) {
+                Pair(trekk.trekkprosent!!, KodeTrekkAlternativ.LOPP)
+            } else {
+                Pair(trekk.trekkbelop, KodeTrekkAlternativ.LOPM)
             }
-        val periode = Periode(trekk.startPeriode, trekk.sluttPeriode, sats)
 
         val innrapporteringTrekk =
             InnrapporteringTrekk(
-                navTrekkId = "nav trekk id",
-                kreditorIdTss = "00987654321",
                 kreditorTrekkId = trekk.trekkid,
                 kontonr = trekk.kontonummer,
                 debitorId = trekk.skyldner,
                 kid = trekk.kid,
                 kreditorsRef = trekk.trekkid,
-                saldo = "1000.00",
-                prioritetFomDato = "prioritet fom dato",
-                gyldigTomDato = "gyldig tomdato",
-                periode = periode,
+                kodeTrekkAlternativ = valueOf(kodeTrekkAlternativ),
+                periode = Periode(trekk.startPeriode, trekk.sluttPeriode, sats),
             )
+    }
+
+    private enum class KodeTrekkAlternativ {
+        LOPM,
+        LOPP,
     }
 }
 
@@ -39,22 +41,57 @@ data class OSUtleggsTrekk(
 
 @Serializable
 @XmlSerialName("innrapporteringTrekk")
+// BLOCK COMMENTS ER FRA CHAT MED ENDRE!!
 data class InnrapporteringTrekk(
+    /*
+         aksjonskode kan ha NY, ENDR (endring av feks sats), ENRS (endring av restsaldo), KANS (kansellering) og OPPH (opphør).
+        Jeg er usikker på når ENRS skal brukes i stedet for ENDR, og når OPPH skal brukes i stedet for KANS. Men dette finner vi ut etterhvert
+     */
     @XmlElement val aksjonskode: String = "NY",
-    @XmlElement val navTrekkId: String,
-    @XmlElement val kreditorIdTss: String,
     @XmlElement val kreditorTrekkId: String,
-    @XmlElement val kreditorOrgnr: String = "00987654321",
     @XmlElement val kontonr: String,
     @XmlElement val debitorId: String,
-    @XmlElement val kodeTrekktype: String = "KRED",
-    @XmlElement val kodeTrekkAlternativ: String = "SALM",
+    // LOPM hvis beløp, LOPP hvis prosent
+    @XmlElement val kodeTrekkAlternativ: String,
     @XmlElement val kid: String,
     @XmlElement val kreditorsRef: String,
-    @XmlElement val saldo: String,
-    @XmlElement val prioritetFomDato: String,
-    @XmlElement val gyldigTomDato: String,
     val periode: Periode,
+    /*
+     * saldo er beløpet som debitor skylder. Brukes ved saldo-trekk, ikke løpende trekk.
+     * Slik at hvis en skylder 10000kr og skal betale 1000pr måned vil felt saldo være lik 10000,
+     * mens sats vil være 1000 og kodeTrekkAlternativ være SALM.
+     *  Hvis de 10000 skal betales med 15 prosent pr måned vil saldo være lik 10000,
+     * mens sats vil være 15 og kodeTrekkAlternativ være SALP.
+     * Men det finnes også trekk som er løpende. Feks husleie.
+     * Der er det ingen saldo (altså = 0), mens sats vil da være månedsleien på feks 5000.
+     * I disse tilfellene er kodeTrekkAlternativ gjerne LOPM.
+     *
+     * Dersom trekkene fra skatt er saldotrekk så må de sende denne.
+     *  Dersom de ikke gjør det vil vi oppfatte trekket som et løpende trekk.
+     *  Og dersom de bare har månedstrekk og ikke saldo vil kodeTrekkAlternativ være LOPM.
+     *
+     *
+     * OVERSATT:
+     * Vi får ikke saldo fra skatt, så disse trekkene anses som løpende
+     * saldo vil derfor ALLTID være 0
+     *
+     * Sats skal settes til likt trekkbeløp eller trekkprosent
+     * ved trekkbeløp: kodeTrekkAlternativ = LOPM
+     * ved trekkprosent: kodeTrekkAlternativ = LOPP
+     * */
+    @XmlElement private val saldo: String = "0.0",
+    @XmlElement private val prioritetFomDato: String = "",
+    @XmlElement private val gyldigTomDato: String = "",
+    @XmlElement private val kreditorIdTss: String = "00987654321",
+    @XmlElement private val kodeTrekktype: String = "KRED",
+    /*
+    navTrekkId er den id'en som OS identifiserer trekket med.
+    Vi trenger ikke sende navTrekkId.
+    Feltet kan brukes til å identifisere trekket ved endringer.
+     Men vi (dere) vil sikkert bruke kombinasjonen kreditorIdTss og kreditorTrekkId.
+     */
+    @XmlElement private val navTrekkId: String = "$kreditorIdTss-$kreditorTrekkId",
+    @XmlElement private val kreditorOrgnr: String = "00987654321",
 )
 
 @Serializable
