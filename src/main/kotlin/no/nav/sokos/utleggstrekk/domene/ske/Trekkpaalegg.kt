2ff37499c8cc382cc90f5fb0f3485cde9a8c1986
KenDahle <ken.dahle@nav.no>
2025-01-10 16:51:16 +0100

    TOB-3845 Refaktorert responseklassen med nye felter og navn. utleggstrekk->paaleggstrekk

---
+++ src/main/kotlin/no/nav/sokos/utleggstrekk/domene/ske/Trekkpaalegg.kt
@@ -0,0 +1,45 @@
+package no.nav.sokos.utleggstrekk.domene.ske
+
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Trekkpaalegg(
+    val trekkid: String,
+    val trekkversjon: Int,
+    val sekvensnummer: Int,
+    val opprettet: LocalDateTime,
+    val saksnummer: String,
+    val trekkpliktig: String,
+    val skyldner: String,
+    val trekkstatus: Trekkstatus,
+    val trekkstoerrelseForPeriode: List<TrekkstorrelseForPeriode>,
+    val kidnummer: String,
+    val kontonummer: String,
+    val betaleTilOrgnummer: String
+)
+
+@Serializable
+data class TrekkProsent(
+    val trekkprosent: Double? = null,
+)
+
+@Serializable
+data class TrekkBeloep(
+    val trekkbeloep: Double? = null,
+)
+
+@Serializable
+enum class Trekkstatus(val value: String){
+    AKTIV("aktiv"),
+    AVSLUTTET("avsluttet")
+}
+
+@Serializable
+data class TrekkstorrelseForPeriode(
+    val startdato: LocalDate,
+    val sluttdato: LocalDate?,
+    val trekkbelop: TrekkBeloep?,
+    val trekkprosent: TrekkProsent?
+)
