25932bca659e3b154702b743f16dfd38c2b7dde1
KenDahle <ken.dahle@nav.no>
2025-01-14 12:23:00 +0100

    TOB-3845 tar ut content negotiation for å se raw tekst av responsen

--- src/main/kotlin/no/nav/sokos/utleggstrekk/config/CommonConfig.kt
+++ src/main/kotlin/no/nav/sokos/utleggstrekk/config/CommonConfig.kt
@@ -1,27 +1,17 @@
 package no.nav.sokos.utleggstrekk.config
 
 import io.ktor.http.HttpHeaders
-import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.auth.Authentication
 import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.jwt.jwt
-import io.ktor.server.metrics.micrometer.MicrometerMetrics
 import io.ktor.server.plugins.callid.CallId
 import io.ktor.server.plugins.callid.callIdMdc
 import io.ktor.server.plugins.calllogging.CallLogging
-import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.request.path
-import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics
-import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
-import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics
-import io.micrometer.core.instrument.binder.system.ProcessorMetrics
-import io.micrometer.core.instrument.binder.system.UptimeMetrics
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.json.Json
 import mu.KotlinLogging
-import no.nav.sokos.utleggstrekk.metrics.Metrics
 import org.slf4j.event.Level
 import java.util.UUID
 
@@ -42,27 +32,17 @@ fun Application.commonConfig(
         filter { call -> call.request.path().startsWith("/utleggstrekk") }
         disableDefaultColors()
     }
-    install(ContentNegotiation) {
-        json(
-            Json {
-                prettyPrint = true
-                isLenient = true
-                explicitNulls = false
-                ignoreUnknownKeys = true
-            },
-        )
-    }
-    install(MicrometerMetrics) {
-        registry = Metrics.registry
-        meterBinders =
-            listOf(
-                UptimeMetrics(),
-                JvmMemoryMetrics(),
-                JvmGcMetrics(),
-                JvmThreadMetrics(),
-                ProcessorMetrics(),
-            )
-    }
+//    install(MicrometerMetrics) {
+//        registry = Metrics.registry
+//        meterBinders =
+//            listOf(
+//                UptimeMetrics(),
+//                JvmMemoryMetrics(),
+//                JvmGcMetrics(),
+//                JvmThreadMetrics(),
+//                ProcessorMetrics(),
+//            )
+//    }
     install(Authentication) {
         jwt {
             verifier(azureConfiguration.azureAd.jwkProvider, azureConfiguration.azureAd.openIdConfiguration.issuer)
