4d6ff702a84ad26415f526137cbb4e4be919ea86
KenDahle <ken.dahle@nav.no>
2025-06-17 12:42:24 +0200

    TOB-3845 Kolonne ikke 0-based

--- src/main/kotlin/no/nav/sokos/utleggstrekk/database/Repository.kt
+++ src/main/kotlin/no/nav/sokos/utleggstrekk/database/Repository.kt
@@ -24,7 +24,7 @@ object Repository {
     fun Connection.fetchLastSekvensnr(): Int {
         val rs = prepareStatement("""select max(sekvensnummer)  from utleggstrekk""").executeQuery()
         return if (rs.next()) {
-            rs.getInt(0)
+            rs.getInt(1)
         } else {
             0
         }
