4b58186be4cdc952e6cca6129839e0f645ec969c
laodice <laodice.melliti@gmail.com>
2025-09-16 12:10:28 +0200

    Lege SlackService og tester

---
+++ src/main/kotlin/no/nav/sokos/utleggstrekk/service/SlackService.kt
@@ -0,0 +1,24 @@
+package no.nav.sokos.utleggstrekk.service
+
+import no.nav.sokos.utleggstrekk.client.SlackClient
+
+data class ErrorMessages(
+    val type: String,
+    val info: List<String>,
+)
+
+class SlackService(
+    private val slackClient: SlackClient = SlackClient(),
+) {
+    suspend fun sendError(
+        header: String,
+        filename: String,
+        vararg errorMessages: ErrorMessages,
+    ) {
+        slackClient.sendMessage(
+            header,
+            filename,
+            errorMessages.associate { (type, info) -> type to info },
+        )
+    }
+}
---
+++ src/test/kotlin/no/nav/sokos/utleggstrekk/service/SlackServiceTest.kt
@@ -0,0 +1,37 @@
+package no.nav.sokos.utleggstrekk.service
+
+import io.kotest.core.spec.style.FunSpec
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import no.nav.sokos.utleggstrekk.client.SlackClient
+
+class SlackServiceTest : FunSpec( {
+    val client = mockk<SlackClient>()
+
+    test("sendError sends one slack message with one error") {
+        coEvery { client.sendMessage(any(), any(), any()) } returns Unit
+
+        val service = SlackService(client)
+
+        service.sendError("header", "filename", ErrorMessages("type", listOf("info")))
+
+        coVerify { client.sendMessage("header", "filename", mapOf("type" to listOf("info"))) }
+    }
+
+    test("sendError sends one slack message with several errors") {
+        coEvery { client.sendMessage(any(), any(), any()) } returns Unit
+
+        val service = SlackService(client)
+
+        val error1 = ErrorMessages("type1", listOf("info11", "info12"))
+        val error2 = ErrorMessages("type2", listOf("info21", "info22"))
+        service.sendError("header", "filename", error1, error2)
+
+        val expectedMessages = mapOf(
+            "type1" to listOf("info11", "info12"),
+            "type2" to listOf("info21", "info22"),
+        )
+        coVerify { client.sendMessage("header", "filename", expectedMessages) }
+    }
+})
