6d8487b2272c23f8fcbf1a9669b115f7bf7cb278
bjornko <bjorn.vaggen.konestabo@nav.no>
2025-12-05 13:39:11 +0100

    Avoid running prepare and grant if not really needed.

--- startProxy.sh
+++ startProxy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ -s "$HOME/.config/gcloud/application_default_credentials.json" ]; then
-# Ensure user is authenicated, and run login if not.
-   gcloud auth print-identity-token &> /dev/null
-   if [ $? -gt 0 ]; then
-      gcloud auth login
-   fi
-   nais postgres prepare sokos-utleggstrekk
-   nais postgres grant sokos-utleggstrekk
-else
-   gcloud auth login --update-adc
+ADC_FILE="$HOME/.config/gcloud/application_default_credentials.json"
+
+if ! gcloud auth application-default print-access-token >/dev/null 2>&1; then
+    echo "ADC not configured or invalid, running login with --update-adc..."
+    gcloud auth login --update-adc
+    nais postgres prepare sokos-utleggstrekk
+    nais postgres grant sokos-utleggstrekk
+fi
+
+if ! gcloud auth print-identity-token >/dev/null 2>&1; then
+    echo "User not logged in, running gcloud auth login..."
+    gcloud auth login
 fi
 
 if nc -z localhost 5432 2>/dev/null; then
