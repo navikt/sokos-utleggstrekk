40d02e35ef88bea867486f4aae88ac450283ac6b
bjornko <bjorn.vaggen.konestabo@nav.no>
2025-12-05 13:07:37 +0100

    Local db proxy script + local proxy POSTGRES_JDBC_URL in setupLocalEnvironment.sh

---
+++ startProxy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Ensure user is authenicated, and run login if not.
+gcloud auth print-identity-token &> /dev/null
+if [ $? -gt 0 ]; then
+    gcloud auth login
+fi
+
+if nc -z localhost 5432 2>/dev/null; then
+    echo "proxy already running"
+else
+    nais postgres prepare sokos-utleggstrekk
+    nais postgres grant sokos-utleggstrekk
+    nais postgres proxy sokos-utleggstrekk
+fi
