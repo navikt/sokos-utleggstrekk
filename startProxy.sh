b683c67beb6d26411fcb36fb412a98323857a1b4
bjornko <bjorn.vaggen.konestabo@nav.no>
2025-12-05 13:32:41 +0100

    Test if --update-adc is necessary.

--- startProxy.sh
+++ startProxy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+if [ -s "$HOME/.config/gcloud/application_default_credentials.json" ]; then
 # Ensure user is authenicated, and run login if not.
-gcloud auth print-identity-token &> /dev/null
-if [ $? -gt 0 ]; then
-    gcloud auth login
+   gcloud auth print-identity-token &> /dev/null
+   if [ $? -gt 0 ]; then
+      gcloud auth login
+   fi
+else
+   gcloud auth login --update-adc
 fi
 
 if nc -z localhost 5432 2>/dev/null; then
