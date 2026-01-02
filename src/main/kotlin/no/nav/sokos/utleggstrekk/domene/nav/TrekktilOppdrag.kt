a0286a1fec7be682958accd959f0a83b735691de
bjornko <bjorn.vaggen.konestabo@nav.no>
2026-01-02 15:08:47 +0100

    TOB-5959 validering.

--- src/main/kotlin/no/nav/sokos/utleggstrekk/domene/nav/TrekktilOppdrag.kt
+++ src/main/kotlin/no/nav/sokos/utleggstrekk/domene/nav/TrekktilOppdrag.kt
@@ -1,6 +1,12 @@
 package no.nav.sokos.utleggstrekk.domene.nav
 
+import java.time.LocalDate
+
 import kotlinx.serialization.Serializable
+
+import no.nav.sokos.utleggstrekk.utils.Validation.isDate
+import no.nav.sokos.utleggstrekk.utils.Validation.isNumber
+import no.nav.sokos.utleggstrekk.utils.Validation.isSafeText
 
 // De er på samme format
 typealias KvitteringFraOppdrag = TrekkTilOppdrag
@@ -89,4 +95,56 @@ enum class TrekkAlternativ(val value: String) {
     ;
 
     val suffix = name[3]
+}
+
+fun TrekkTilOppdrag.validate() {
+    dokument.validate()
+    mmel?.validate()
+}
+
+fun Mmel.validate() {
+    require(alvorlighetsgrad.isSafeText()) { "alvorlighetsgrad har ugyldige tegn" }
+    if (systemId != null) require(systemId.isSafeText()) { "systemId har ugyldige tegn" }
+    if (kodeMelding != null) require(kodeMelding.isSafeText()) { "kodeMelding har ugyldige tegn" }
+    if (beskrMelding != null) require(beskrMelding.isSafeText()) { "beskrMelding har ugyldige tegn" }
+    if (sqlKode != null) require(sqlKode.isSafeText()) { "sqlKode har ugyldige tegn" }
+    if (sqlStateMmel != null) require(sqlStateMmel.isSafeText()) { "sqlStateMmel har ugyldige tegn" }
+    if (sqlMelding != null) require(sqlMelding.isSafeText()) { "sqlMelding har ugyldige tegn" }
+    if (mqCompletionKode != null) require(mqCompletionKode.isSafeText()) { "mqCompletionKode har ugyldige tegn" }
+    if (mqReasonKode != null) require(mqReasonKode.isSafeText()) { "mqReasonKode har ugyldige tegn" }
+    if (programId != null) require(programId.isSafeText()) { "programId har ugyldige tegn" }
+    if (sectionNavn != null) require(sectionNavn.isSafeText()) { "sectionNavn har ugyldige tegn" }
+}
+
+fun Document.validate() {
+    require(transaksjonsId.isSafeText()) { "transaksjonsId har ugyldige tegn" }
+    innrapporteringTrekk.validate()
+}
+
+fun InnrapporteringTrekk.validate() {
+    require(navTrekkId.isSafeText()) { "navTrekkId har ugyldige tegn" }
+    require(kreditorIdTss.length <= 11) { "kreditorIdTss er ugydlig" }
+    require(kreditorTrekkId.length <= 35 && kreditorTrekkId.isSafeText()) { "kreditorTrekkId er ugyldig" }
+    require(kreditorsRef.length <= 30 && kreditorsRef.isSafeText()) { "kreditorsRef er ugyldig" }
+    require(debitorId.length == 11 && debitorId.isNumber()) { "debitorId er ugyldig" }
+    require(kodeTrekktype.length <= 4 && kodeTrekktype.isSafeText()) { "kodeTrekkType er ugyldig" }
+    require(kid.length <= 26 && kid.isSafeText()) { "kid er ugyldig" }
+    require(kilde.length <= 11 && kilde.isSafeText()) { "kilde er ugyldig" }
+    require(saldo >= 0.0) { "saldo < 0" }
+    if (prioritetFomDato != null) require(prioritetFomDato.isDate()) { "prioritetFomDato er ugyldig" }
+    if (gyldigTomDato != null) require(gyldigTomDato.isDate()) { "gyldigTomDato er ugyldig" }
+    perioder?.validate()
+}
+
+fun Perioder.validate() {
+    periode.forEach(Periode::validate)
+}
+
+fun Periode.validate() {
+    require(periodeFomDato.isDate()) { "periodeFomDato er ugyldig" }
+    if (periodeTomDato != null) {
+        val fom = LocalDate.parse(periodeFomDato)
+        val tom = LocalDate.parse(periodeTomDato)
+        require(fom.isBefore(tom)) { "Periodeintervall er ugyldig" }
+    }
 }
