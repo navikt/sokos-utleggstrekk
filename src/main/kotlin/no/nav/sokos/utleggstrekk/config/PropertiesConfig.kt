eb75c01f1226cb82e58c1043266c2737ebcb1122
laodice <laodice.melliti@gmail.com>
2026-01-22 11:05:25 +0100

    TOB-5944: Ha alle data classes relatert til applikasjonen config i samme fil

--- src/main/kotlin/no/nav/sokos/utleggstrekk/config/PropertiesConfig.kt
+++ src/main/kotlin/no/nav/sokos/utleggstrekk/config/PropertiesConfig.kt
@@ -1,5 +1,8 @@
 package no.nav.sokos.utleggstrekk.config
 
+import kotlinx.serialization.Serializable
+
+import com.nimbusds.jose.jwk.RSAKey
 import com.typesafe.config.ConfigFactory
 import io.ktor.server.config.ApplicationConfig
 import io.ktor.server.config.HoconApplicationConfig
@@ -64,3 +67,84 @@ fun ApplicationConfig.mergeWithEnv(): ApplicationConfig {
 }
 
 infix fun ApplicationConfig.overriding(other: ApplicationConfig): ApplicationConfig = this.withFallback(other)
+
+enum class Profile {
+    LOCAL,
+    DEV,
+    TEST,
+    PROD,
+}
+
+@Serializable
+data class ApplicationProperties(
+    val profile: Profile,
+    val appName: String,
+    val namespace: String,
+    val naisPodName: String,
+    val scheduler: SchedulerProperties = SchedulerProperties(),
+) {
+    val isLocal = profile == Profile.LOCAL
+    val isTest = profile == Profile.TEST
+}
+
+@Serializable
+data class SchedulerProperties(
+    val isActive: Boolean = false,
+    val minutes: Int = 45,
+)
+
+@Serializable
+data class SkeConfig(
+    val skeRestUrl: String,
+    val skeOrgNr: String,
+    val skeKontoNr: String,
+    val skeTSSId: String,
+)
+
+@Serializable
+data class MaskinportenClientConfig(
+    val clientId: String,
+    val wellKnownUrl: String,
+    val scopes: String,
+    val rsaKeyString: String,
+    val systemBrukerClaim: String,
+) {
+    val rsaKey: RSAKey? by lazy {
+        RSAKey.parse(rsaKeyString)
+    }
+}
+
+@Serializable
+data class MQProperties(
+    val hostname: String,
+    val port: Int,
+    val mqQueueManagerName: String,
+    val mqChannelName: String,
+    val queueName: String,
+    val replyQueueName: String,
+    val username: String,
+    val password: String,
+    val userAuth: Boolean,
+)
+
+@Serializable
+data class PostgresConfig(
+    val host: String,
+    val port: String,
+    val name: String,
+    val username: String,
+    val password: String,
+    val jdbcUrl: String,
+    val user: String,
+)
+
+@Serializable
+data class SlackConfig(val url: String)
+
+@Serializable
+data class UnleashProperties(
+    val unleashApi: String = "",
+    val apiKey: String = "",
+    val environment: String = "",
+    val enabled: Boolean,
+)
--- src/main/kotlin/no/nav/sokos/utleggstrekk/config/SlackConfig.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.sokos.utleggstrekk.config
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class SlackConfig(val url: String)
