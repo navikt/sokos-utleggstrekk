69c1337850f23b347843a18bd08c657ac43a2a3f
Laodice <laodice.melliti@gmail.com>
2026-01-30 11:01:31 +0100

    TOB-6145: Bytte ut apache med apache5 (#138)

--- src/main/kotlin/no/nav/sokos/utleggstrekk/client/HttpClient.kt
+++ src/main/kotlin/no/nav/sokos/utleggstrekk/client/HttpClient.kt
@@ -5,15 +5,15 @@ import java.net.ProxySelector
 import kotlinx.serialization.json.Json
 
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
+import io.ktor.client.engine.apache5.Apache5
 import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
-import org.apache.http.impl.conn.SystemDefaultRoutePlanner
+import org.apache.hc.client5.http.impl.routing.SystemDefaultRoutePlanner
 
 // TODO: Sett json config her
 val httpClient =
-    HttpClient(Apache) {
+    HttpClient(Apache5) {
         expectSuccess = false
 
         install(HttpRequestRetry) {
