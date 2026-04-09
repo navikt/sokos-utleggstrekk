aaa32d93dca8e715d82d559a5083dd2ea67dea48
Bjørn <bjorn.vaggen.konestabo@nav.no>
2026-04-09 09:27:15 +0200

    Update Kotlin version in build.gradle.kts
    Keep codeql happy

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 plugins {
-    kotlin("jvm") version "2.3.20"
-    kotlin("plugin.serialization") version "2.3.20"
+    kotlin("jvm") version "2.3.10"
+    kotlin("plugin.serialization") version "2.3.10"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
     id("org.jetbrains.kotlinx.kover") version "0.9.8"
 
