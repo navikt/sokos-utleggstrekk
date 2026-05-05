fa5194dcaf6d5fd480fc37a9ea384fe2f24f4b1c
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2026-05-05 13:13:53 +0200

    Revert "Kast CancellationException videre uten å forsøke å gjenkøe"
    This reverts commit 4e2bd60e90c9232f1ab6dcd57a801a70a69692af.

--- src/main/kotlin/no/nav/sokos/utleggstrekk/service/SlackService.kt
+++ src/main/kotlin/no/nav/sokos/utleggstrekk/service/SlackService.kt
@@ -1,6 +1,5 @@
 package no.nav.sokos.utleggstrekk.service
 
-import kotlin.coroutines.cancellation.CancellationException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.SupervisorJob
@@ -80,8 +79,6 @@ class SlackService(private val slackClient: SlackClient = SlackClient()) {
 
         try {
             slackClient.sendMessage(messageTitle, errorsToSend)
-        } catch (cancellation: CancellationException) {
-            throw cancellation
         } catch (exception: Exception) {
             errorsToSend.forEach { (type, info) ->
                 info.forEach { addErrorSuspending(type, it) }
