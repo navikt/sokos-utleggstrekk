2231bd56692468b7721de23cafb929a380e6cb08
Thea Marie Alnæs <thea.marie.alnes@nav.no>
2026-06-23 13:26:46 +0200

    Reverted oppgradering av jvm og plugin.serialization for codeql taklet det ikke

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 plugins {
-    kotlin("jvm") version "2.4.0"
-    kotlin("plugin.serialization") version "2.4.0"
+    kotlin("jvm") version "2.3.21"
+    kotlin("plugin.serialization") version "2.3.21"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
     id("org.jetbrains.kotlinx.kover") version "0.9.8"
 
