8bacf82ea041c61ecb3fd718b39bd405cd066eb9
takwaiwang <tak.wai.wang@nav.no>
2026-08-18 13:20:57 +0200

    TOB-6993 legg til trekkpaalegg validateringsfeil årsak

--- src/test/kotlin/no/nav/sokos/utleggstrekk/domene/ske/ValidationTest.kt
+++ src/test/kotlin/no/nav/sokos/utleggstrekk/domene/ske/ValidationTest.kt
@@ -3,6 +3,8 @@ package no.nav.sokos.utleggstrekk.domene.ske
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 import io.ktor.server.config.ApplicationConfig
 import io.mockk.clearAllMocks
 import io.mockk.every
@@ -67,26 +69,30 @@ class ValidationTest :
         Given("Trekkpålegg med ugyldige feltverdier") {
             val ugyldige =
                 listOf(
-                    trekkpaalegg.copy(trekkid = "1232\t"),
-                    trekkpaalegg.copy(sekvensnummer = -1),
-                    trekkpaalegg.copy(trekkversjon = -1),
-                    trekkpaalegg.copy(opprettet = "2026-01-01"),
-                    trekkpaalegg.copy(saksnummer = "sak\n"),
-                    trekkpaalegg.copy(trekkpliktig = "trekkpliktig"),
-                    trekkpaalegg.copy(skyldner = "bob"),
-                    trekkpaalegg.copy(trekkstoerrelseForPeriode = listOf(trekkpaalegg.trekkstoerrelseForPeriode.first().copy(startdato = "342"))),
-                    trekkpaalegg.copy(trekkstoerrelseForPeriode = listOf(trekkpaalegg.trekkstoerrelseForPeriode.first().copy(sluttdato = "342"))),
-                    trekkpaalegg.copy(trekkstoerrelseForPeriode = listOf(trekkpaalegg.trekkstoerrelseForPeriode.first().copy(trekkbeloep = Trekkbeloep(-1000.0)))),
-                    trekkpaalegg.copy(trekkstoerrelseForPeriode = listOf(trekkpaalegg.trekkstoerrelseForPeriode.first().copy(trekkbeloep = null, trekkprosent = Trekkprosent(110.0)))),
-                    trekkpaalegg.copy(trekkstoerrelseForPeriode = listOf(trekkpaalegg.trekkstoerrelseForPeriode.first().copy(trekkbeloep = null, trekkprosent = Trekkprosent(-20.0)))),
-                    trekkpaalegg.copy(betalingsinformasjon = trekkpaalegg.betalingsinformasjon.copy(betalingsmottaker = "1123131111221")),
-                    trekkpaalegg.copy(betalingsinformasjon = trekkpaalegg.betalingsinformasjon.copy(kidnummer = "112hfd1")),
-                    trekkpaalegg.copy(betalingsinformasjon = trekkpaalegg.betalingsinformasjon.copy(kontonummer = "konto")),
+                    trekkpaalegg.copy(trekkid = "1232\t") to "Trekkpaalegg validation failed: trekkid: 1232\t, trekkversjon: 1, sekvensnummer: 1, message: trekkid har ugyldige tegn",
+                    trekkpaalegg.copy(sekvensnummer = -1) to "message: sekvensnummer har ulovlig verdi",
+                    trekkpaalegg.copy(trekkversjon = -1) to "message: trekkversjon har ulovlig verdi",
+                    trekkpaalegg.copy(opprettet = "2026-01-01") to "message: opprettet har ulovlig verdi",
+                    trekkpaalegg.copy(saksnummer = "sak\n") to "message: saksnummer har ugyldige tegn",
+                    trekkpaalegg.copy(trekkpliktig = "trekkpliktig") to "message: trekkpliktig har ulovlig verdi",
+                    trekkpaalegg.copy(skyldner = "bob") to "message: skyldner har ulovlig verdi",
+                    trekkpaalegg.copy(trekkstoerrelseForPeriode = listOf(trekkpaalegg.trekkstoerrelseForPeriode.first().copy(startdato = "342"))) to "message: Startdato har ulovlig verdi",
+                    trekkpaalegg.copy(trekkstoerrelseForPeriode = listOf(trekkpaalegg.trekkstoerrelseForPeriode.first().copy(sluttdato = "342"))) to "message: Sluttdato har ulovlig verdi",
+                    trekkpaalegg.copy(trekkstoerrelseForPeriode = listOf(trekkpaalegg.trekkstoerrelseForPeriode.first().copy(trekkbeloep = Trekkbeloep(-1000.0)))) to
+                        "message: Trekkbeloep ulovlig verdi",
+                    trekkpaalegg.copy(trekkstoerrelseForPeriode = listOf(trekkpaalegg.trekkstoerrelseForPeriode.first().copy(trekkbeloep = null, trekkprosent = Trekkprosent(110.0)))) to
+                        "message: Trekkprosent har ulovlig verdi",
+                    trekkpaalegg.copy(trekkstoerrelseForPeriode = listOf(trekkpaalegg.trekkstoerrelseForPeriode.first().copy(trekkbeloep = null, trekkprosent = Trekkprosent(-20.0)))) to
+                        "message: Trekkprosent har ulovlig verdi",
+                    trekkpaalegg.copy(betalingsinformasjon = trekkpaalegg.betalingsinformasjon.copy(betalingsmottaker = "1123131111221")) to "message: Betalingsmottaker har ulovlig verdi",
+                    trekkpaalegg.copy(betalingsinformasjon = trekkpaalegg.betalingsinformasjon.copy(kidnummer = "112hfd1")) to "message: kidnummer har ulovlig verdi",
+                    trekkpaalegg.copy(betalingsinformasjon = trekkpaalegg.betalingsinformasjon.copy(kontonummer = "konto")) to "message: kontonummer har ulovlig verdi",
                 )
             Then("Validate kaster exception") {
-                ugyldige.forEachIndexed { i, ugyldigTrekk ->
+                ugyldige.forEachIndexed { i, (ugyldigTrekk, expected) ->
                     withClue("TREKK nr. $i, $ugyldigTrekk skal ikke validere") {
-                        shouldThrow<Exception> { ugyldigTrekk.validate() }
+                        val errorMessage = shouldThrow<Exception> { ugyldigTrekk.validate() }
+                        errorMessage.message shouldContain expected
                     }
                 }
             }
@@ -113,23 +119,24 @@ class ValidationTest :
         Given("Utleggstrekk med ugyldige feltverdier") {
             val ugyldige =
                 listOf(
-                    utleggstrekk.copy(navTrekkId = "123\t34"),
-                    utleggstrekk.copy(kreditorIdTss = "abc\n"),
-                    utleggstrekk.copy(kreditorTrekkId = "fjd\t"),
-                    utleggstrekk.copy(debitorId = "fdsafsd\t"),
-                    utleggstrekk.copy(kodeTrekktype = "234237943"),
-                    utleggstrekk.copy(kid = "fdsa\t"),
-                    utleggstrekk.copy(kilde = "fsdfdjsaiofdsajfsfs"),
-                    utleggstrekk.copy(saldo = -2.3),
-                    utleggstrekk.copy(prioritetFomDato = "fdsaifuh"),
-                    utleggstrekk.copy(gyldigTomDato = "fdsaifuh"),
-                    utleggstrekk.copy(perioder = Perioder(listOf(Periode("test", null, sats = 2000.0)))),
-                    utleggstrekk.copy(perioder = Perioder(listOf(Periode("2002-11-10", null, sats = -1.0)))),
+                    utleggstrekk.copy(navTrekkId = "123\t34") to "navTrekkId har ugyldige tegn",
+                    utleggstrekk.copy(kreditorIdTss = "abc\n") to "kreditorIdTss er ugyldig",
+                    utleggstrekk.copy(kreditorTrekkId = "fjd\t") to "kreditorTrekkId er ugyldig",
+                    utleggstrekk.copy(debitorId = "fdsafsd\t") to "debitorId er ugyldig",
+                    utleggstrekk.copy(kodeTrekktype = "234237943") to "kodeTrekkType er ugyldig",
+                    utleggstrekk.copy(kid = "fdsa\t") to "kid er ugyldig",
+                    utleggstrekk.copy(kilde = "fsdfdjsaiofdsajfsfs") to "kilde er ugyldig",
+                    utleggstrekk.copy(saldo = -2.3) to "saldo < 0",
+                    utleggstrekk.copy(prioritetFomDato = "fdsaifuh") to "prioritetFomDato er ugyldig",
+                    utleggstrekk.copy(gyldigTomDato = "fdsaifuh") to "gyldigTomDato er ugyldig",
+                    utleggstrekk.copy(perioder = Perioder(listOf(Periode("test", null, sats = 2000.0)))) to "periodeFomDato er ugyldig",
+                    utleggstrekk.copy(perioder = Perioder(listOf(Periode("2002-11-10", null, sats = -1.0)))) to "sats er ugyldig",
                 )
             Then("validate kaster exception") {
-                ugyldige.forEachIndexed { i, ugyldigTrekk ->
+                ugyldige.forEachIndexed { i, (ugyldigTrekk, expected) ->
                     withClue("TREKK nr $i, $ugyldigTrekk skal ikke validere ") {
-                        shouldThrow<Exception> { ugyldigTrekk.validate() }
+                        val errorMessage = shouldThrow<Exception> { ugyldigTrekk.validate() }
+                        errorMessage.message shouldBe expected
                     }
                 }
             }
